8581c92160f81b4ae9cc71affe7dcc60b12d3c6b
sandhiya <sandhiya070497@gmail.com>
2025-06-08 20:44:12 +0530

    first

--- index.js
+++ index.js
@@ -25,4 +25,7 @@ app.post("/api/login", (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Backend running at http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
